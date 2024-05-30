cbc10068a4a53384dd2f97af82c877524a7bdec4
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-05-30 15:07:56 +0530

    Update script.js

--- Project 7/script.js
+++ Project 7/script.js
@@ -1,1 +1,53 @@
+function convertTemperature(){
 
+
+
+
+
+    const celsiusInput = document.getElementById("celsius");
+
+    const fahrenheitInput = document.getElementById("fahrenheit");
+
+
+
+
+
+    if(!isNaN(celsiusInput.value)){
+
+
+
+        const celsiusValue = parseFloat(celsiusInput.value);
+
+        const fahrenheitValue = (celsiusValue * 9/5) + 32;
+
+
+
+        fahrenheitInput.value = fahrenheitValue.toFixed(2);
+
+
+
+    }else if(!isNaN(fahrenheitInput.value)){
+
+        const fahrenheitValue = fahrenheitInput.value;
+
+        const celsiusValue = (fahrenheitValue - 32) * 5/9;
+
+
+
+        celsiusInput.value = celsiusValue.toFixed(2);
+
+
+
+    }
+
+    else{
+
+        alert("Please enter a valid number")
+
+    }
+
+
+
+
+
+}
