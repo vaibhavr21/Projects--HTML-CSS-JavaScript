ee0d605ee2bd7f32928b642f6628f65bd77a4979
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-06-04 16:11:39 +0530

    Update script.js

--- Project 6/script.js
+++ Project 6/script.js
@@ -1,95 +1,50 @@
-const timeline = document.getElementById("timeline");
+function drag() {
+    let dragging = false;
+    let mouseX, mouseY;
+    let eleX, eleY;
 
-const prev = document.getElementById("prev");
+    const boxes = document.querySelectorAll("[draggable]");
 
-const next = document.getElementById("next");
+    boxes.forEach(box => {
+        box.addEventListener('mousedown',mouseDown);
+        box.style.top = 0;
+        box.style.left = 0;
+    });
 
-const circles = document.querySelectorAll(".circle");
+    function mouseDown(e) {
+        e.preventDefault();
+
+        dragging = this;
+
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+
+        eleX = Number.parseInt(dragging.style.left);
+        eleY = Number.parseInt(dragging.style.top);
+
+        document.addEventListener("mousemove", mouseMove);
+        document.addEventListener("mouseup", mouseUp);
 
 
+    }
 
-let step = 1;
+    function mouseMove(e) {
+        if(dragging){
+            const deltaMouseX = e.pageX - mouseX;
+            const deltaMouseY = e.pageY - mouseY;
 
+            dragging.style.left = deltaMouseX + eleX + "px";
+            dragging.style.top = deltaMouseY + eleY + "px";
 
-
-function mainCode(){
-
-
-
-    circles.forEach((circles,index)=>{
-
-        if(index < step){
-
-            circles.classList.add("active");
-
-        }else{
-
-            circles.classList.remove("active");
 
         }
 
-    });
+    }
+    function mouseUp(e) {
+        dragging = false;
 
-
-
-    const actives = document.querySelectorAll(".active");
-
-    timeline.style.width = ` ${
-
-        ((actives.length - 1) / (circles.length - 1)) * 100
-
-}%`;
-
-
-
-if(step === 1){
-
-    prev.disabled = true;
-
-}else if( step === circles.length){
-
-    next.disabled = true;
-
-}else{
-
-    prev.disabled = false;
-
-    next.disabled = false;
-
-}
-
-
-
-}
-
-
-
-next.addEventListener("click",()=>{
-
-    step++;
-
-    if(step > circles.length){
-
-        step = circles.length;
 
     }
+}
 
-    mainCode();
-
-});
-
-
-
-prev.addEventListener("click",()=>{
-
-    step--;
-
-    if(step < 1){
-
-        step = 1;
-
-    }
-
-    mainCode();
-
-});    
+drag();
