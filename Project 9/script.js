1ed6dd473a42c3ecc7ac3ee0107f45085a328acd
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-06-24 08:14:55 +0530

    Update script.js

--- Project 9/script.js
+++ Project 9/script.js
@@ -1,1 +1,27 @@
+const button = document.getElementById("copy");
+const main_paragraph = document.getElementById("main_paragraph");
+const message = document.getElementById("message");
 
+function copyToClipboard(element){
+    
+    navigator.clipboard.writeText(element.textContent).then(()=>{
+        message.textContent = "Copied to clipboard";
+        message.classList.add("after-clicked");
+    
+    })
+    .catch((err)=>{
+
+        console.log("Unable to Copy the Data");
+    })
+    .finally(()=>{
+        setTimeout(()=>{
+            message.textContent = "";
+            message.classList.remove("after-clicked");
+
+        },2000);
+    });
+
+
+}
+
+button.addEventListener("click",()=>copyToClipboard(main_paragraph));
