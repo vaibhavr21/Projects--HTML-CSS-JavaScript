fd053bdecaad2cd15158897057581875b48b6b86
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-06-25 01:11:35 +0000

    script.js updated

--- Project 12/script.js
+++ Project 12/script.js
@@ -1,21 +1,21 @@
 const testimonials = [ 
     {
-        name: "Bijay Kumar",
-        position: "Creator of Info with Vijay",
+        name: "Mayank Kumar",
+        position: "Creator of VKM",
         image: "https://cdn.pixabay.com/photo/2016/04/22/04/57/graduation-1345143_640.png",
         testimonial: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt ex et lacus pulvinar, eu fermentum sapien volutpat. Nullam non ullamcorper elit.`,
       },
   
       {
-        name: "Dino Code Acadmey",
+        name: "Vayu Tech",
         position: "Tutorial Channel",
         image: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359554_640.png",
         testimonial: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt ex et lacus pulvinar, eu fermentum sapien volutpat. Nullam non ullamcorper elit.`,
       },
 
       {
-        name:"Vijay Kumar",
-        position: "Founder of the Dino Code Academy",
+        name:"Vaibhav Kumar",
+        position: "Founder of VKM",
         image:"https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_640.png",
         testimonial:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt ex et lacus pulvinar, eu fermentum sapien volutpat. Nullam non ullamcorper elit.`,
     },
